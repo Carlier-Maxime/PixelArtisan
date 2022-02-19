90a723d6da20d788e5a7add32f4a2aaf2ba6897d
Maxime Carlier <maxime_carlier@ens.univ-artois.fr>
2022-02-19 16:38:50 +0100

    command '/pa create' place acacia_log (temporary)

--- src/fr/metouais/pixelartisan/commands/CreateCommand.java
+++ src/fr/metouais/pixelartisan/commands/CreateCommand.java
@@ -1,13 +1,20 @@
 package fr.metouais.pixelartisan.commands;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public class CreateCommand extends MyCommand{
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender instanceof Player player){
+            Block block = player.getLocation().getBlock();
+            block.setType(Material.ACACIA_LOG);
+        }
         sender.sendMessage("'/"+label+" create' executed !");
         return false;
     }
