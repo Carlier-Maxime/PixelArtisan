9b0be0883b4e7f4080226d562cae107df465f795
M_EtOuais <28m06c02@gmail.com>
2025-04-11 18:36:19 +0200

    :recycle: refactor global paths

--- common/src/main/java/fr/metouais/pixelartisan/PixelArtisan.java
+++ common/src/main/java/fr/metouais/pixelartisan/PixelArtisan.java
@@ -10,10 +10,11 @@ import java.util.concurrent.Executors;
 public class PixelArtisan {
     public static PixelArtisan instance;
     public static final Logger LOGGER = LoggerFactory.getLogger(PixelArtisan.class);
-    public static final Path PATH_INPUT_TEXTURE = Path.of("plugins/PixelArtisan/input_texture");
-    public static final Path PATH_IMAGES = Path.of("plugins/PixelArtisan/images");
-    public static final Path PATH_DATA = Path.of("plugins/PixelArtisan/data");
-    public static final Path PATH_DEBUG = Path.of("plugins/PixelArtisan/debug");
+    private static final Path PATH_PREFIX = Path.of("plugins/PixelArtisan");
+    public static final Path PATH_INPUT_TEXTURE = PATH_PREFIX.resolve("input_texture");
+    public static final Path PATH_IMAGES = PATH_PREFIX.resolve("images");
+    public static final Path PATH_DATA = PATH_PREFIX.resolve("data");
+    public static final Path PATH_DEBUG = PATH_PREFIX.resolve("debug");
     public static final Path[] PATHS = {PATH_INPUT_TEXTURE, PATH_IMAGES, PATH_DATA, PATH_DEBUG};
     public static final String GIT_LINK = "https://github.com/Carlier-Maxime/PixelArtisan";
     private final ExecutorService executorService = Executors.newCachedThreadPool();
