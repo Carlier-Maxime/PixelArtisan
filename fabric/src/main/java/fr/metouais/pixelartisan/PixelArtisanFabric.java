d700802d1ad58f7b4aad344e427bef45110dffac
M_EtOuais <28m06c02@gmail.com>
2025-04-11 23:33:51 +0200

    :construction: prepare module fabric

---
+++ fabric/src/main/java/fr/metouais/pixelartisan/PixelArtisanFabric.java
@@ -0,0 +1,10 @@
+package fr.metouais.pixelartisan;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PixelArtisanFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Pixel Artisan Fabric");
+    }
+}
