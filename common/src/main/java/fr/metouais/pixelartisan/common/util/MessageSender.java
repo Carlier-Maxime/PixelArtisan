2ce83020358c484189898fd212c4ce8a3a480bf3
M_EtOuais <28m06c02@gmail.com>
2025-04-15 20:08:59 +0200

    :bug: fix color bug on msg send with PREFIX_WITH_COLOR

--- common/src/main/java/fr/metouais/pixelartisan/common/util/MessageSender.java
+++ common/src/main/java/fr/metouais/pixelartisan/common/util/MessageSender.java
@@ -4,7 +4,7 @@ import fr.metouais.pixelartisan.common.PixelArtisan;
 
 public interface MessageSender {
     String PREFIX = "["+Info.NAME+"] ";
-    String PREFIX_WITH_COLOR = "§3"+PREFIX+"§r";
+    String PREFIX_WITH_COLOR = "§3["+Info.NAME+"]§r ";
     MessageSender CONSOLE = msg -> PixelArtisan.LOGGER.info("{}{}", PREFIX, msg);
     void send(String msg);
 }
