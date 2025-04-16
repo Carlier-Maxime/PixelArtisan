122561a4566697035bfcaa6987386d74ad8deb1d
M_EtOuais <28m06c02@gmail.com>
2025-04-17 00:50:55 +0200

    :sparkles: enable '/pa debug listMaterial'

--- common/src/main/java/fr/metouais/pixelartisan/common/command/DebugCommand.java
+++ common/src/main/java/fr/metouais/pixelartisan/common/command/DebugCommand.java
@@ -1,8 +1,16 @@
 package fr.metouais.pixelartisan.common.command;
 
+import fr.metouais.pixelartisan.common.PixelArtisan;
+import fr.metouais.pixelartisan.common.block.Block;
+import fr.metouais.pixelartisan.common.block.IBlock;
 import fr.metouais.pixelartisan.common.command.base.Command;
 import fr.metouais.pixelartisan.common.command.base.CommandFactory;
 import fr.metouais.pixelartisan.common.util.MessageSender;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
 
 public class DebugCommand {
     private static Command command;
@@ -10,35 +18,30 @@ public class DebugCommand {
     public static Command get() {
         if (command == null) {
             command = CommandFactory.builder("debug")
-                .subcommand(CommandFactory.builder("listMaterial")
-                    .execute((sender, args) -> listAllMaterials(sender))
+                .subcommand(CommandFactory.builder("listBlocks")
+                    .execute((sender, args) -> listAllBlocks(sender))
                 );
         }
         return command;
     }
 
-    private static void listAllMaterials(MessageSender sender) {
-        //TODO
-        sender.send("list all materials");
-        /*int count = 0;
+    private static void listAllBlocks(MessageSender sender) {
+        int count = 0;
         final int LIMIT = 64;
-        Path file = PixelArtisan.PATH_DEBUG.resolve("listMaterial.txt");
+        Path file = PixelArtisan.PATH_DEBUG.resolve("listBlock.txt");
         try {
-            Files.write(file.toFile().toPath(),
-                    Arrays.stream(Misc.MATERIALS)
-                            .map(Material::name)
-                            .collect(Collectors.toList()));
+            Files.write(file.toFile().toPath(), Block.all().stream().map(IBlock::getId).collect(Collectors.toList()));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        for (Material mat : Misc.MATERIALS) {
-            sender.send( "§7- " + mat.name());
+        for (var block : Block.all()) {
+            sender.send( "§7- " + block.getId());
             count++;
             if (count >= LIMIT) {
                 sender.send( "§e... list truncated to "+LIMIT+" elements, to see the whole list consult the file : "+file);
                 break;
             }
-        }*/
+        }
     }
 }
